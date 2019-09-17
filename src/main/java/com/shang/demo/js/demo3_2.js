f56f17782a7d1c5e47d4ae985a48953ad96c2f67
尚家朋 <shawn@qishimai.cn>
2019-09-17 18:32:55 +0800

    学习js文件,执行流程

---
+++ src/main/java/com/shang/demo/js/demo3_2.js
@@ -0,0 +1,2 @@
+var demo=require('./demo3_1');
+console.log(demo.add(400,600));
---
+++ src/main/java/com/shang/demo/js/demo4.js
@@ -0,0 +1,12 @@
+var http = require('http');
+http.createServer(function (request, response) {
+    //发送http头部
+    //http状态值 200:OK
+    //内容类型;text/
+
+}).listen(8888)
+
+
+wx.login = function (request, response) {
+    js_code = callback()
+};
